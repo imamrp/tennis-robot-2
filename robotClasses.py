5380a1cdc8ddc531bdf994531201c02a0200fd1f
imamrp <153504850+imamrp@users.noreply.github.com>
2024-09-03 17:43:44 +1000

    Got rid of oscillation prevention
    So that the motor can stop faster

--- robotClasses.py
+++ robotClasses.py
@@ -173,12 +173,6 @@ class DiffDriveRobot:
         duty_cycle_L = min(max(-100,prev_cycle_L + delta_duty_cycle_L),100)
         duty_cycle_R = min(max(-100,prev_cycle_R + delta_duty_cycle_R),100)
         
-        if (duty_cycle_L*wL_desired < 0): #If duty cycle and desired w are in oposite directions (prevents wheel from oscillating)
-            duty_cycle_L = 0
-
-        if (duty_cycle_R*wR_desired < 0): #If duty cycle and desired w are in oposite directions (prevents wheel from oscillating)
-            duty_cycle_R = 0
-        
         # Error accumulation
         error_sum_L = error_sum_L + (wL_desired-wL_measured)
         error_sum_R = error_sum_R + (wR_desired-wR_measured)
