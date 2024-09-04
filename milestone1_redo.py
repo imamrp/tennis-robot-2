486cdf537423d313f51f2f7c2660b235b92f791d
imam <imprakoso03@gmail.com>
2024-09-04 15:40:05 +1000

    Added alignment code with PI controller; TODO: test it

--- milestone1_redo.py
+++ milestone1_redo.py
@@ -6,27 +6,30 @@ This is a redo of the Milestone 1 task of the tennis robot
 from robotClasses import DiffDriveRobot
 
 
-def go_to_ball(center:int, Kp:float, Ki:float, sum_error:int):
+def allign_to_ball(ball_center:int, sum_error:int, desired_center=340, Kp=0.1, Ki=0.01):
     """Function will return a forward velocity and the angular velocity needed to go towards a ball.
     To determine the angular velocity, this function uses a PI controller.
 
     Args:
-        center (int): Horizontal position of the ball in the frame.
+        ball_center (int): Current horizontal position of the ball in the frame.
+        sum_error (int): The accumulative sum of the error.
+        desired_center (int=340): The desired center of the ball
         Kp (float): The proportional gain.
         Ki (float): The integral gain.
-        sum_error (int): The accumulative sum of the error.
 
     Returns:
         Tuple(float,float): The desired forward velocity and the desired angular velocity 
     """
-    # Set the forward speed
-    v_desired = 0.05
     
     # Find error
-    error =  - center
+    error = desired_center - ball_center
     
+    # PI controller for the desired w (limited to +/-1 rad/s)
+    w_desired = min(max(-1,Kp*error + Ki*sum_error),1)
     
-    return v_desired, w_desired, sum_error
+    sum_error += error
+    
+    return w_desired, sum_error
 
 def milestone1_process(v_desired,w_desired):
     '''Stage 1: go to center'''
@@ -42,6 +45,7 @@ def milestone1_process(v_desired,w_desired):
     
     
     '''Stage 5: Traverse to start'''
+    pass
 
 def robot_control_process(v_desired,w_desired):
     """The process that controls the robot's motors continuously and repeatedly.
@@ -53,4 +57,7 @@ def robot_control_process(v_desired,w_desired):
     
     while True:
         duty_cycle_L, duty_cycle_R, wL_desired, wL_measured, wR_desired, wR_measured = robot.drive(v_desired=v_desired.value, w_desired=w_desired.value)        
+
+
+if __name__ == "__main__":
     
