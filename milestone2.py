05bda3f0d98075cc1ef7e249d902ce96826de0e4
imamrp <153504850+imamrp@users.noreply.github.com>
2024-09-06 16:43:07 +1000

    Basic structure of milestone 2 created

--- milestone2.py
+++ milestone2.py
@@ -1,2 +1,84 @@
 from robotClasses import DiffDriveRobot
-import detection
+import detection, multiprocessing, time
+
+def update_ball_center(center, radius): # Takes approximately 0.2s to process one frame
+    """Function (or process) that updates the center of the ball and its radius in the camera frame.
+
+    Args:
+        center (int): The center of the ball in the frame (340 is exactly center, >340 is to the right of the frame, <340 is the left)
+        radius (int): The radius of the largest ball in pixels.
+
+    Returns:
+        None
+    """
+    print("Ball detection process initiated...\n\n\n")
+    detector = detection.TennisBallDetector()
+    while True:
+        _, frame = detector.cap.read()
+        detected_balls = detector.process_frame(frame)
+        detected_center = detector.get_circle_1_center(detected_balls)
+        detected_radius = detector.get_circle_1_radius(detected_balls)
+        if detected_center:
+            center.value = detected_center[0]
+            radius.value = detected_radius
+        else:
+            center.value = -1
+            radius.value = -1
+        # print(f"X: {center.value} R: {radius.value}")
+
+def robot_control_process(v_desired,w_desired, theta):
+    """The process that controls the robot's motors continuously and repeatedly.
+
+    Args:
+        v_desired (float): The desired speed of the robot
+    """
+    robot = DiffDriveRobot()
+    print("Robot motor control process initiated...\n\n\n")
+    while True:
+        duty_cycle_L, duty_cycle_R, wL_desired, wL_measured, wR_desired, wR_measured = robot.drive(v_desired=v_desired.value, w_desired=w_desired.value) 
+        theta.value = robot.th
+
+def milestone2_process(v_desired, w_desired, center, radius, theta):
+    """The process that controls the finite state machine (FSM) for the milestone 2 task. States are numbered with integers as follows:
+        State 0: State where the robot is first activated. Transitions: 
+        State 1:
+        State 2:
+        ... TODO: add other states
+
+    Args:
+        v_desired (float): The desired speed of the robot
+        w_desired (float): The desired rotational speed of the robot speed of the robot
+        center (int): The center of the ball in the frame (340 is exactly center, >340 is to the right of the frame, <340 is the left)
+        radius (int): The radius of the largest ball in pixels.
+        theta (float): The angle of the robot with respect to the starting position. Positive is counter clockwise.
+    """
+
+    """Setup"""
+    state = 0
+    """While loop FSM"""
+    while True:
+
+if __name__ == "__main__":
+    '''Start the update_ball_center process'''
+    center = multiprocessing.Value('i', -1)
+    radius = multiprocessing.Value('i', -1)
+    center_process = multiprocessing.Process(target=update_ball_center, args=(center, radius))
+    center_process.start()
+
+    '''Start process for motor control'''
+    v_desired = multiprocessing.Value('f', 0)
+    w_desired = multiprocessing.Value('f', 0)
+    theta = multiprocessing.Value('f', 0)
+    motor_ctrl_process = multiprocessing.Process(target=robot_control_process, args=(v_desired, w_desired, theta))
+    motor_ctrl_process.start()
+
+
+    '''Main process'''
+    main_process = multiprocessing.Process(target=milestone2_process, args=(v_desired, w_desired, center, radius, theta))
+    main_process.start()
+
+    '''Join processes'''
+    motor_ctrl_process.join()
+    center_process.join()
+    main_process.join()
+    
