7a05fa6cd02ea22b194d0456a7953e1454080195
aste0049 <aste0049@student.monash.edu>
2024-09-09 15:23:44 +1000

    same as last

--- milestone1_redo.py
+++ milestone1_redo.py
@@ -155,7 +155,7 @@ def milestone1_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
     error = angle_to_turn
     print('rotating robot')
     while error > 0.01:
-        error, w_desired = rotate_robot(start_theta = start_theta, robot_theta = theta.value, angle_to_turn = angle_to_turn, Kp = 6e-2)
+        error, w_desired.value = rotate_robot(start_theta = start_theta, robot_theta = theta.value, angle_to_turn = angle_to_turn, Kp = 6e-2)
         print('w_desired: ', w_desired)
 
     # rotate_robot(w_desired = w_desired.value, robot_theta = theta.value, angle_to_turn = np.pi/2)
