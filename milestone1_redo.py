07eb9fe59bf294891442d21b03c91cd442d22289
aste0049 <aste0049@student.monash.edu>
2024-09-09 15:43:26 +1000

    adding a print comment to test if while loop exited

--- milestone1_redo.py
+++ milestone1_redo.py
@@ -163,6 +163,7 @@ def milestone1_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
     # w_desired.value = 0
 
     # moving 1 meter forward
+    print('moving forward')
     move_forward(v_deisred = v_desired.value, robot_x = robot_x.value, robot_y = robot_y.value, dist = 1)
     v_desired.value = 0
 
