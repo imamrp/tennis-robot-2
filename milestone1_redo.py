cb77943e56baa8b13d58e1adb890c583297cda64
aste0049 <aste0049@student.monash.edu>
2024-09-11 12:21:42 +1000

    testing right turning

--- milestone1_redo.py
+++ milestone1_redo.py
@@ -158,7 +158,7 @@ def milestone1_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
     #     error, w_desired.value = rotate_robot(start_theta = start_theta, robot_theta = theta.value, angle_to_turn = angle_to_turn)
     #     print('w_desired: ', w_desired)
     #     print('theta: ', theta.value)
-    w_desired.value = 0.262
+    w_desired.value = -0.262
     time.sleep(6.0)
     w_desired.value = 0
     print("Sleep 10 sec")
