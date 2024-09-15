9eae01ae1019d384422ade5d41e5fe895c9f2e14
aina0004 <aina0004@student.monash.edu>
2024-09-15 12:52:10 +1000

    sensor multiplexing

--- testing/dual_rgb_test.py
+++ testing/dual_rgb_test.py
@@ -1,42 +1,55 @@
 import time
 import board
-import busio
 import adafruit_tcs34725
 import RPi.GPIO as GPIO
 
-i2c1 = busio.I2C(scl=board.D7, sda=board.D8)
-i2c2 = busio.I2C(scl=board.D11, sda=board.D10)
-
-sensor1 = adafruit_tcs34725.TCS34725(i2c1)
-sensor2 = adafruit_tcs34725.TCS34725(i2c2)
+SENSOR1_POWER_PIN = 17
+SENSOR2_POWER_PIN = 27
 
 LED_PIN = 4
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-led_state = False
-GPIO.output(LED_PIN, led_state)
 
-def toggle_led():
-    global led_state
-    led_state = not led_state
-    GPIO.output(LED_PIN, led_state)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SENSOR1_POWER_PIN, GPIO.OUT)
+GPIO.setup(SENSOR2_POWER_PIN, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+GPIO.output(SENSOR1_POWER_PIN, GPIO.LOW)
+GPIO.output(SENSOR2_POWER_PIN, GPIO.LOW)
+
+def init_sensor():
+    i2c = board.I2C()
+    sensor = adafruit_tcs34725.TCS34725(i2c)
+    sensor.integration_time = 2.4
+    sensor.gain = 60
+    return sensor
 
 def read_sensor(sensor):
     r, g, b = sensor.color_rgb_bytes
     return r, g, b
 
-sensor1.integration_time = 2.4
-sensor1.gain = 60
-sensor2.integration_time = 2.4
-sensor2.gain = 60
+led_state = False
+def toggle_led():
+    global led_state
+    led_state = not led_state
+    GPIO.output(LED_PIN, led_state)
 
 toggle_led()
 
 while True:
-    r1, g1, b1 = read_sensor(sensor1)
-    print('Sensor 1 Color: ({0}, {1}, {2})'.format(r1, g1, b1))
+    # Turn on Sensor 1 and read data
+    GPIO.output(SENSOR1_POWER_PIN, GPIO.HIGH)
+    GPIO.output(SENSOR2_POWER_PIN, GPIO.LOW)
+    time.sleep(0.1)
+    sensor1 = init_sensor()
+    r, g, b = read_sensor(sensor1)
+    print('Sensor 1 Color: ({0}, {1}, {2})'.format(r, g, b))
+    time.sleep(0.1)
 
-    r2, g2, b2 = read_sensor(sensor2)
-    print('Sensor 2 Color: ({0}, {1}, {2})'.format(r2, g2, b2))
-
-    time.sleep(0.5)
+    # Turn on Sensor 2 and read data
+    GPIO.output(SENSOR1_POWER_PIN, GPIO.LOW)
+    GPIO.output(SENSOR2_POWER_PIN, GPIO.HIGH)
+    time.sleep(0.1)
+    sensor2 = init_sensor()
+    r, g, b = read_sensor(sensor2)
+    print('Sensor 2 Color: ({0}, {1}, {2})'.format(r, g, b))
+    time.sleep(0.1)
