f3020351d9c1179d119ab209e24e34b5c1e9a046
aste0049 <aste0049@student.monash.edu>
2024-09-23 12:28:45 +1000

    Changing ultrasonic test

--- testing/ultrasonic.py
+++ testing/ultrasonic.py
@@ -14,23 +14,11 @@ GPIO.setup(ECHO,GPIO.IN)
 GPIO.output(TRIG, False)
 sensor = gpiozero.DistanceSensor(echo=ECHO,trigger=TRIG)
 print ("Waiting For Sensor To Settle")
-time.sleep(2)
+time.sleep(1)
 
 # measuring
-for _ in range(1000):
-    # start ranging program
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)     # 10 us
-    GPIO.output(TRIG, False)
-    while GPIO.input(ECHO)==0:
-        pulse_start = time.time()
-    while GPIO.input(ECHO)==1:
-        pulse_end = time.time()
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150 
-    distance = round(distance, 2)
-    print ("Distance:",distance,"cm")
+for j in range(100):
     print('Distance: ', sensor.distance * 100)
-    time.sleep(0.1)  
+    time.sleep(0.5)
 
 GPIO.cleanup() 
