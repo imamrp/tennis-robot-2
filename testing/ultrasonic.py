81a54e0f2fbccae6ef32fe32a7df072c2f45c59a
aste0049 <aste0049@student.monash.edu>
2024-09-23 12:34:40 +1000

    ultrasonic debugging

--- testing/ultrasonic.py
+++ testing/ultrasonic.py
@@ -3,15 +3,9 @@ import RPi.GPIO as GPIO
 import time
 
 # setup
-GPIO.setmode(GPIO.BCM)
-
 TRIG = 3
 ECHO = 2
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
-GPIO.output(TRIG, False)
 sensor = gpiozero.DistanceSensor(echo=ECHO,trigger=TRIG)
 print ("Waiting For Sensor To Settle")
 time.sleep(1)
