2ce5df46155ad95d7899d8ca0c71933c1c1a563d
aste0049 <aste0049@student.monash.edu>
2024-09-25 17:20:09 +1000

    Added file to test servo motion

---
+++ testing/servo.py
@@ -0,0 +1,20 @@
+from gpiozero import Servo, PWMOutputDevice
+from time import sleep
+
+# Connect servo to GPIO11
+PWM_pin = 11
+servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.002,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
+
+for j in range(10):
+    servo.value = -1
+    print('min value')
+    sleep(1)
+    servo.value = 0
+    print('min value')
+    sleep(1)
+    servo.value = 1
+    print('min value')
+    sleep(1)
+
+# send servo to pos 0
+servo.value = 0
