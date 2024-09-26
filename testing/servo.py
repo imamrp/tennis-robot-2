e7b6b2cadabc4ebdab0e60c2532597db8fca0019
imamrp <153504850+imamrp@users.noreply.github.com>
2024-09-26 12:07:13 +1000

    testing servo

--- testing/servo.py
+++ testing/servo.py
@@ -6,7 +6,7 @@ PWM_pin = 11
 servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.002,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
 
 def ctrl_gate(servo, open):
-    servo.value = 1 if open else -1 # Open gate
+    servo.value = -1 if open else 1 # Open gate
     sleep(1)
 
 # for j in range(2):
