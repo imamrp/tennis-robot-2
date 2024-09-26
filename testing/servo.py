ea1972a6584334a5f72fe7548cfbb14a91615250
aina0004 <109999809+aina0004@users.noreply.github.com>
2024-09-26 11:29:00 +1000

    Update servo.py

--- testing/servo.py
+++ testing/servo.py
@@ -6,15 +6,12 @@ PWM_pin = 11
 servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.002,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
 
 for j in range(10):
-    servo.value = -1
-    print('min value')
-    sleep(1)
-    servo.value = 0
-    print('min value')
-    sleep(1)
     servo.value = 1
-    print('min value')
+    print('Close')
+    sleep(3)
+    servo.value = -1
+    print('Open')
     sleep(1)
 
 # send servo to pos 0
-servo.value = 0
+servo.value = 1
