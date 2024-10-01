ef0f388827a0469aa3700c27474d0832432f7660
imamrp <153504850+imamrp@users.noreply.github.com>
2024-10-01 10:48:51 +1000

    Update servo.py

--- testing/servo.py
+++ testing/servo.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # Connect servo to GPIO11
 PWM_pin = 11
-servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.002,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
+servo = Servo(PWM_pin,min_pulse_width=0.0008, max_pulse_width=0.002,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
 
 def ctrl_gate(servo, open):
     servo.value = -1 if open else 1 # Open gate
