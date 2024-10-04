e7282ca5052b75254f6738e4da8c3de1f0177e98
aste0049 <aste0049@student.monash.edu>
2024-10-04 16:50:34 +1000

    changing search direction

--- milestone2.py
+++ milestone2.py
@@ -110,6 +110,7 @@ def milestone2_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
     """Setup"""
     state = 1
     balls_collected = 0
+    search_direction = 1
     GPIO.setmode(GPIO.BCM)
 
     # servo setup
@@ -155,7 +156,7 @@ def milestone2_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
             print("State 1: Finding ball====================================\n\n\n")
             use_cam.value = 1
             print('camera activated: ',use_cam.value)
-            states.state1(w_desired, center)
+            states.state1(w_desired, center, search_direction)
             state = 2
         
         #### State 2: Align and move towards ball ####
@@ -168,6 +169,7 @@ def milestone2_process(v_desired, w_desired, center, radius, rotbot_x, robot_y, 
                 use_cam.value = 0
             else:
                 state = 1
+                search_direction = search_direction * -1
             
         #### State 3: Collecting ball ####
         elif state == 3:
