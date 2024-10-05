3fd9f2ed36c6617ffa7078cde849cc1efa9d770d
imamrp <153504850+imamrp@users.noreply.github.com>
2024-10-05 14:55:21 +1000

    DEBUGGING: added args

--- states.py
+++ states.py
@@ -257,7 +257,7 @@ def state4(robot_x, robot_y, theta, w_desired, v_desired, left_line_detected, ri
     desired_rotaion = np.pi - theta.value
     rotate_robot(w_desired, theta, angle_to_turn = desired_rotation)
     
-def state5(v_desired, w_desired, box_distance, left_line_detected, right_line_detected):
+def state5(robot_x, robot_y, theta, v_desired, w_desired, box_distance, left_line_detected, right_line_detected):
     '''
     follow the line until close to the box
 
