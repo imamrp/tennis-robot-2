2167f3a8670106faec281a77a7769553caa42de9
aste0049 <aste0049@student.monash.edu>
2024-10-07 16:23:41 +1100

    servo test

--- testing/servo.py
+++ testing/servo.py
@@ -3,13 +3,13 @@ from time import sleep
 
 # Connect servo to GPIO11
 PWM_pin = 11
-servo = Servo(PWM_pin,min_pulse_width=0.0008, max_pulse_width=0.0022,frame_width=0.0025)    # min and max pulse width may need to be changed if rom not large enough
+servo = Servo(PWM_pin,min_pulse_width=0.002, max_pulse_width=0.0044,frame_width=0.005)    # min and max pulse width may need to be changed if rom not large enough
 
 def ctrl_gate(servo, open):
     servo.value = -1 if open else 1 # Open gate
     sleep(1)
 
-for j in range(2):
+for j in range(5):
     print("close")
     ctrl_gate(servo,open=False)
     sleep(3)
