a3f68ee569fadff0a9cb892611285f791bafab5a
aste0049 <aste0049@student.monash.edu>
2024-10-07 16:31:16 +1100

    servo test

--- testing/servo.py
+++ testing/servo.py
@@ -3,23 +3,44 @@ from time import sleep
 
 # Connect servo to GPIO11
 PWM_pin = 11
-servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.0044,frame_width=0.005)    # min and max pulse width may need to be changed if rom not large enough
+# setup the GPIO pin for the servo
+servo_pin = 11
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin,GPIO.OUT)
 
-def ctrl_gate(servo, open):
-    servo.value = -1 if open else 1 # Open gate
-    sleep(1)
+# setup PWM process
+pwm = GPIO.PWM(servo_pin,50) # 50 Hz (20 ms PWM period)
 
-for j in range(5):
-    print("close")
-    ctrl_gate(servo,open=False)
-    sleep(3)
-    print('Open')
-    ctrl_gate(servo,open=True)
-    sleep(1)
-    print(j)
+pwm.start(7) # start PWM by rotating to 90 degrees
 
-# send servo to pos 0
-ctrl_gate(servo, False)
-while True:
-    pass
+for i in range(5):
+    pwm.ChangeDutyCycle(2.0) # rotate to 0 degrees
+    time.sleep(0.5)
+    pwm.ChangeDutyCycle(12.0) # rotate to 180 degrees
+    time.sleep(0.5)
+    pwm.ChangeDutyCycle(7.0) # rotate to 90 degrees
+    time.sleep(0.5)
+
+pwm.ChangeDutyCycle(0) # this prevents jitter
+pwm.stop() # stops the pwm on 13
+GPIO.cleanup() # good practice when finished using a pin
+# servo = Servo(PWM_pin,min_pulse_width=0.001, max_pulse_width=0.0044,frame_width=0.005)    # min and max pulse width may need to be changed if rom not large enough
+
+# def ctrl_gate(servo, open):
+#     servo.value = -1 if open else 1 # Open gate
+#     sleep(1)
+
+# for j in range(5):
+#     print("close")
+#     ctrl_gate(servo,open=False)
+#     sleep(3)
+#     print('Open')
+#     ctrl_gate(servo,open=True)
+#     sleep(1)
+#     print(j)
+
+# # send servo to pos 0
+# ctrl_gate(servo, False)
+# while True:
+#     pass
 
