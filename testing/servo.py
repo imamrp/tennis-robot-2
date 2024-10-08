1dea353d8768d4badb3f6e84e3148abdd851bc07
aste0049 <aste0049@student.monash.edu>
2024-10-08 11:27:49 +1100

    servo test

--- testing/servo.py
+++ testing/servo.py
@@ -10,14 +10,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin,GPIO.OUT)
 
 # setup PWM process
-pwm = GPIO.PWM(servo_pin,100) # 50 Hz (20 ms PWM period)
+pwm = GPIO.PWM(servo_pin,50) # 50 Hz (20 ms PWM period)
 
 pwm.start(7) # start PWM by rotating to 90 degrees
 
 for i in range(2):
-    pwm.ChangeDutyCycle(24.0) # rotate to 0 degrees
+    pwm.ChangeDutyCycle(12.0) # rotate to 0 degrees
     time.sleep(3)
-    pwm.ChangeDutyCycle(8.0) # rotate to 180 degrees
+    pwm.ChangeDutyCycle(4.0) # rotate to 180 degrees
     time.sleep(3)
 
 pwm.ChangeDutyCycle(0) # this prevents jitter
